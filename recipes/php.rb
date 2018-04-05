96a845eb276472fc21920e51af67c23dded32c8a
Kostas Gliozeris <kostasmgl@gmail.com>
2018-04-05 16:25:33 +0100

    Update php.rb

--- recipes/php.rb
+++ recipes/php.rb
@@ -9,6 +9,16 @@ node.set['php']['version'] = node['cookbook_totara']['default_config']['php']['v
 node.set['php-fpm']['pools'] = node['cookbook_totara']['default_config']['php-fpm']['pools']
 node.set['php']['packages'] = node['cookbook_totara']['default_config']['php']['packages']
 
+node.set['php']['conf_dir'] = node['cookbook_totara']['default_config']['php']['conf_dir']
+node.set['php']['curl'] = node['cookbook_totara']['default_config']['php']['curl']
+node.set['php']['gd'] = node['cookbook_totara']['default_config']['php']['gd']
+node.set['php']['fpm_package'] = node['cookbook_totara']['default_config']['php']['fpm_package']
+node.set['php']['fpm_pooldir'] = node['cookbook_totara']['default_config']['php']['fpm_pooldir']
+node.set['php']['fpm_service'] = node['cookbook_totara']['default_config']['php']['fpm_service']
+node.set['php']['fpm_socket'] = node['cookbook_totara']['default_config']['php']['fpm_socket']
+node.set['php']['fpm_default_conf'] = node['cookbook_totara']['default_config']['php']['fpm_default_conf']
+node.set['php']['ext_conf_dir'] = node['cookbook_totara']['default_config']['php']['ext_conf_dir']
+
 apt_repository 'ondrej-php' do
   uri          'ppa:ondrej/php'
   trusted      true
