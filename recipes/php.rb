3004f322e3abe68e5c165cda5018fc3f0f1801fd
Kostas Gliozeris <kostasmgl@gmail.com>
2018-04-05 13:18:31 +0100

    Update php.rb

--- recipes/php.rb
+++ recipes/php.rb
@@ -9,5 +9,13 @@ node.set['php']['version'] = node['cookbook_totara']['default_config']['php']['v
 node.set['php-fpm']['pools'] = node['cookbook_totara']['default_config']['php-fpm']['pools']
 node.set['php']['packages'] = node['cookbook_totara']['default_config']['php']['packages']
 
+apt_repository 'ondrej-php' do
+  uri          'ppa:ondrej/php'
+  distribution node['lsb']['codename']
+  only_if      {
+    node['php']['version'] >= '7'
+  }
+end
+
 include_recipe "php"
 include_recipe "php-fpm"
