0d397ca0623c46245e01463fdabba8ed534efc51
Kostas Gliozeris <kostasmgl@gmail.com>
2018-04-06 10:58:30 +0100

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -11,30 +11,30 @@ default['cookbook_totara']['cron_hour'] = "*"
 default['cookbook_totara']['cron_minute'] = "*/5"
 
 default['cookbook_totara']['htpasswd'] = {
-  'username': '',
-  'password': '',
-  'path': ''
+  'username' => '',
+  'password' => '',
+  'path' => ''
 }
 
 default['cookbook_totara']['default_config']['nginx'] = {
-  'template_name': 'nginx_vhost_totara.conf.erb',
-  'template_cookbook': 'cookbook_totara',
-  'listen_port': '80',
-  'variables': {}
+  'template_name' => 'nginx_vhost_totara.conf.erb',
+  'template_cookbook' => 'cookbook_totara',
+  'listen_port' => '80',
+  'variables' => {}
 }
 
 default['cookbook_totara']['default_config']['php'] = {
-  'version': '7.2',
-  'conf_dir': '/etc/php/7.2/cli',
-  'curl': {'package': 'php7.2-curl'},
-  'gd': {'package': 'php7.2-gd'},
-  'fpm_package': 'php7.2-fpm',
-  'fpm_pooldir': '/etc/php/7.2/fpm/pool.d',
-  'fpm_service': 'php7.2-fpm',
-  'fpm_socket': '/var/run/php/php7.2-fpm.sock',
-  'fpm_default_conf': '/etc/php/7.2/fpm/pool.d/www.conf',
-  'ext_conf_dir': '/etc/php/7.2/mods-available',
-  'packages': %w(php7.2-cgi php7.2 php7.2-dev php7.2-cli php-pear php7.2-mysql php7.2-gd php7.2-mbstring php7.2-zip php7.2-curl php7.2-intl php7.2-xmlrpc php7.2-soap)
+  'version' => '7.2',
+  'conf_dir' => '/etc/php/7.2/cli',
+  'curl' => {'package' => 'php7.2-curl'},
+  'gd' => {'package' => 'php7.2-gd'},
+  'fpm_package' => 'php7.2-fpm',
+  'fpm_pooldir' => '/etc/php/7.2/fpm/pool.d',
+  'fpm_service' => 'php7.2-fpm',
+  'fpm_socket' => '/var/run/php/php7.2-fpm.sock',
+  'fpm_default_conf' => '/etc/php/7.2/fpm/pool.d/www.conf',
+  'ext_conf_dir' => '/etc/php/7.2/mods-available',
+  'packages' => %w(php7.2-cgi php7.2 php7.2-dev php7.2-cli php-pear php7.2-mysql php7.2-gd php7.2-mbstring php7.2-zip php7.2-curl php7.2-intl php7.2-xmlrpc php7.2-soap)
 }
 
 default['cookbook_totara']['default_config']['php-fpm']['pools'] = {
@@ -64,8 +64,6 @@ default['cookbook_totara']['default_config']['php-fpm']['error_log'] = '/var/log
 default['cookbook_totara']['default_config']['php-fpm']['pid'] = '/var/run/php7.2-fpm.pid'
 
 default['cookbook_totara']['mysql'] = {
-  'version': '5.7',
-  'configuration_template': 'default.cnf.erb'
+  'version' => '5.7',
+  'configuration_template' => 'default.cnf.erb'
 }
-
-
