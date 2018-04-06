b96a7065377c8d288a3390e551d324356866ef6a
Kostas Gliozeris <kostasmgl@gmail.com>
2018-04-06 11:18:58 +0100

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -25,6 +25,7 @@ default['cookbook_totara']['default_config']['nginx'] = {
 
 default['cookbook_totara']['default_config']['php'] = {
   'version' => '7.2',
+  'pear' => 'pear',
   'conf_dir' => '/etc/php/7.2/cli',
   'curl' => {'package' => 'php7.2-curl'},
   'gd' => {'package' => 'php7.2-gd'},
