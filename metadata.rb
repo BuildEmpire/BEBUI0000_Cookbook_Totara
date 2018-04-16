5f25db855f9537cd239d2029d963ee8a4eec6a90
Kostas Gliozeris <kostasmgl@gmail.com>
2018-04-16 10:53:54 +0100

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'michael.wright@buildempire.co.uk'
 license          'Apache 2.0'
 description      'Totara cookbook.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '0.1.0'
 
 recipe 'cookbook_totara', 'Totara Cookbook'
 
