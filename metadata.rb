45938ce2402cead775dc435159574530b76d2e3b
Jonny Barnes <jonny@jonnybarnes.uk>
2018-04-20 10:46:57 +0100

    Update metadata.rb
    Bump version number

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'michael.wright@buildempire.co.uk'
 license          'Apache 2.0'
 description      'Totara cookbook.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.2'
+version          '1.0.3'
 
 recipe 'cookbook_totara', 'Totara Cookbook'
 
