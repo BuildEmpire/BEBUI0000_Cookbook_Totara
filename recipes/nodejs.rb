86d9a0913c6abfb226c7122b33382cdf33363e5a
Jonny Barnes <jonathan.barnes@buildempire.co.uk>
2018-06-25 14:04:05 +0100

    Comment why specified nodejs version

--- recipes/nodejs.rb
+++ recipes/nodejs.rb
@@ -6,7 +6,7 @@
 #
 
 node.default['nodejs']['install_method'] = 'binary'
-node.default['nodejs']['version'] = '8.11.3'
+node.default['nodejs']['version'] = '8.11.3' # Latest LTS release on 2018-06-25
 node.default['nodejs']['binary']['checksum'] = '1ea408e9a467ed4571730e160993f67a100e8c347f6f9891c9a83350df2bf2be'
 include_recipe "nodejs"
 
